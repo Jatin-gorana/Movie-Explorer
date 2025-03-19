5a69ee289efa83f164d9fc0707b1ceca84fccf71
Jatin_Gorana <jatingorana123@gmail.com>
2025-03-19 19:30:03 +0530

    fi

--- src/components/ProtectedContent.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import ProtectedRoute from './ProtectedRoute';
-
-export default function ProtectedContent({ children }: { children: React.ReactNode }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-} 
